73739686bdd3c781faa62c1b8830e46d025333e6
Thangcamap <161563681+Thangcamap@users.noreply.github.com>
2025-09-03 15:50:00 +0700

    hh

---
+++ web/backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Route test
+app.get("/", (req, res) => {
+  res.send("🚀 Backend is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
