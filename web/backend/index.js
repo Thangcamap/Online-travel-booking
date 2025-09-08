afee7a4380d773c42a9115f2d93e485c8617e4b2
Thangcamap <161563681+Thangcamap@users.noreply.github.com>
2025-09-08 22:19:30 +0700

    update

--- web/backend/index.js
+++ web/backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const { pool } = require("./config/mysql");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Route test
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  
+const t =  await pool.query("SELECT 1 + 1 AS solution")
+console.log('The solution is: ', t[0][0].solution); // should log "The solution is: 2"
   res.send("🚀 Backend is running...");
 });
 
