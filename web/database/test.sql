0928c05bc71e0b36c4a11f807aacd73017d5e79f
Thangcamap <161563681+Thangcamap@users.noreply.github.com>
2025-11-01 00:14:28 +0700

    update tk admin

--- web/database/test.sql
+++ web/database/test.sql
@@ -21,6 +21,14 @@ ALTER TABLE tour_itineraries CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_un
 SET foreign_key_checks = 1;
 
 
+--  chạy câu lênh sau để tạo tài khoản admin ban đầu
+admin@gmail.com 
+password: admin
+tài khoản đăng nhập để vào trang admin
 
-INSERT INTO users (name, email, password, role)
-VALUES ('admin', 'admin@gmail.com', '$2b$10$hs40kZ1tOuUqu.WSCoOISuXA8cEov661oM2HcV2w8ZH2bS51vjMFC', 'admin');
+INSERT INTO users (user_id, name, email, password, phone_number, role)
+VALUES ('ADM001', 'Admin', 'admin@gmail.com', '$2b$10$hs40kZ1tOuUqu.WSCoOISuXA8cEov661oM2HcV2w8ZH2bS51vjMFC', '0999999999', 'admin');
+
+INSERT INTO admins (admin_id, user_id)
+VALUES ('A001', 'ADM001');
+
